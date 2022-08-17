2a1e6b33cf90b3895f9f5c7e0876c296a7ebe9f7
Sri Kirsh <skirsh@mozilla.com>
2022-08-16 19:30:48 -0700

    fix: stop logging client errors and NOT_FOUND errors in sentry & cloudwatch (#197)
    * stop logging client errors and NOT_FOUND errors
    
    * change hardcoded prefix to static property
    
    * use originalError property to check NotFoundError
    
    * lint fix

--- src/plugins/sentryPlugin.ts
+++ src/plugins/sentryPlugin.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/node';
-import { ApolloError } from 'apollo-server-errors';
 import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+import { NotFoundError } from '../errorHandler/errorHandler';
 
 /**
  * Plugin for handling errors.
@@ -18,6 +18,16 @@ export const sentryPlugin: ApolloServerPlugin = {
                  to request-specific lifecycle events. */
     return {
       async didEncounterErrors(ctx) {
+        //for error codes:
+        // https://www.apollographql.com/docs/apollo-server/data/errors/#bad_user_input
+        const errorCodes = [
+          'FORBIDDEN',
+          'UNAUTHENTICATED',
+          'BAD_USER_INPUT',
+          'GRAPHQL_PARSE_FAILED',
+          'GRAPHQL_VALIDATION_FAILED',
+        ];
+
         // If we couldn't parse the operation, don't
         // do anything here
         if (!ctx.operation) {
@@ -27,7 +37,10 @@ export const sentryPlugin: ApolloServerPlugin = {
         for (const err of ctx.errors) {
           // Only report internal server errors,
           // all errors extending ApolloError should be user-facing
-          if (err instanceof ApolloError) {
+          if (
+            errorCodes.includes(err.extensions?.code?.toString()) ||
+            err.originalError instanceof NotFoundError
+          ) {
             continue;
           }
 
